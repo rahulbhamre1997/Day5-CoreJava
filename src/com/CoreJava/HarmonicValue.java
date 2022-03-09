53fd8252e79852224ace7ffb43f343cca69e2efc
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-03-09 14:16:19 +0530

    Added:Print Harmonic Value

---
+++ src/com/CoreJava/HarmonicValue.java
@@ -0,0 +1,22 @@
+package com.CoreJava;
+
+import java.util.Scanner;
+
+public class HarmonicValue {
+	public static void main(String[] args) {
+		 System.out.print("Enter any number : ");
+		   Scanner sc = new Scanner(System.in);
+		   int num = sc.nextInt();
+		System.out.println("The Harmonic Number is : ");
+		   double result = 0;
+		   while(num > 0)
+		{
+			result = result + (double) 1 / num;
+			num--;
+			System.out.println(result +"  ");
+		}
+		System.out.println("");
+		System.out.println("Output of Harmonic Series is "+result);
+		sc.close();
+	}
+}
