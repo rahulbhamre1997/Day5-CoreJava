f249e0cf68ea5c3d0d365312522948be08aaf7b8
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-03-09 14:58:18 +0530

    Added:Check Alphabet is vowel or consonant

---
+++ src/com/CoreJava/Vowel.java
@@ -0,0 +1,16 @@
+package com.CoreJava;
+
+public class Vowel {
+
+	public static void main(String[] args) {
+		
+		char ch = 'c';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+
+}
