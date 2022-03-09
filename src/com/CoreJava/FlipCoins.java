a6f69b4eda440424905301160ff33e78ad9f4ffb
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-03-09 13:33:20 +0530

    Added:FlipCoinsLogic

--- src/com/CoreJava/FlipCoins.java
+++ src/com/CoreJava/FlipCoins.java
@@ -1,9 +1,29 @@
 package com.CoreJava;
 
+import java.util.Scanner;
+
 public class FlipCoins {
 
+	public static void headTail() {
+		System.out.println("Give a number: ");
+		Scanner sc = new Scanner(System.in);
+		int headCount = 0;
+		int tailCount = 0;
+		int count = sc.nextInt();
+		for (int i = 0; i < count; i++) {
+			double random = Math.random(); 
+			if (random < 0.5)
+				tailCount++;
+			else
+				headCount++;
+		}
+		System.out.println("percentage of tails("+ tailCount +"): " + (tailCount * 100f / (headCount+tailCount)));
+		System.out.println("percentage of heads("+ headCount +"): " + (headCount * 100f / (headCount+tailCount)));
+		sc.close();
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to FlipCoins Game");
+		headTail();
+	
 	}
 
 }
